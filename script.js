6723afc7467779ed05355daccbd3c8e05c705518
Muhammad Ali <muhammadali16032003@gmail.com>
2026-03-09 21:30:34 +0500

    JS file updated

--- script.js
+++ script.js
@@ -1,8 +1,29 @@
-let tasks = [];
+let tasks = JSON.parse(localStorage.getItem('tasks')) || [];
+
+function renderTasks() {
+  document.getElementById('list').innerHTML = '';
+  tasks.forEach((task, index) => {
+    document.getElementById('list').innerHTML += `
+      <li>
+        ${task}
+        <button onclick="deleteTask(${index})">Delete</button>
+      </li>
+    `;
+  });
+}
 
 function addTask() {
   const task = document.getElementById('task').value;
   tasks.push(task);
-  document.getElementById('list').innerHTML += `<li>${task}</li>`;
+  localStorage.setItem('tasks', JSON.stringify(tasks));
+  renderTasks();
   document.getElementById('task').value = '';
 }
+
+function deleteTask(index) {
+  tasks.splice(index, 1);
+  localStorage.setItem('tasks', JSON.stringify(tasks));
+  renderTasks();
+}
+
+renderTasks();
